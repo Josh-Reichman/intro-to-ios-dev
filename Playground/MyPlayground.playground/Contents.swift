f8d031f6c6fddfc4d9840bf74321b268e484328a
Josh-Reichman <whodaman101@gmail.com>
2018-01-22 10:45:54 -0500

    Init Commit playground

---
+++ Playground/MyPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var optionalStr : String?
+
+var optionalInt : Int? = 9
+
+let actualInt = optionalInt
