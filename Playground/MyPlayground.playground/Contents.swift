3c35dfe9b76080a4714d893c7fbcb5a3596f7f02
Josh-Reichman <whodaman101@gmail.com>
2018-01-22 11:10:28 -0500

    a

--- Playground/MyPlayground.playground/Contents.swift
+++ Playground/MyPlayground.playground/Contents.swift
@@ -1,7 +1,7 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
-
+//NOTE: let = constant, var = variable
 var str = "Hello, playground"
 
 var optionalStr : String?
@@ -13,9 +13,25 @@ let actualInt : Int = optionalInt!
 //Does not work! see below
 //let actualString : String = optionalStr!
 
-if let actualString = optionalStr {
+//NOTE: from class: if let actualString = optionalStr
+if optionalStr != nil {
     
 }
 
 var myStr = "7"
 var possibleInt = Int(myStr) //it assumes it is an optional Int
+
+var ratingList = [ "poor", "fine", "good", "excellent"]
+
+ratingList[0]
+ratingList[0] = "bad"
+
+
+var emptyArray = [String]()
+
+var test : Bool = true
+
+let t = (first:1, second:"First")
+
+let temp = t.0
+let temp2 = t.first
