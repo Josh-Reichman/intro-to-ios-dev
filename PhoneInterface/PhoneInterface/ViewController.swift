1876e29927ef5c812ad8f67eb0b0b2f9d4e8534d
Josh-Reichman <whodaman101@gmail.com>
2018-01-24 11:01:14 -0500

    Created phone interface app. Logic not implemented yet

---
+++ PhoneInterface/PhoneInterface/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  PhoneInterface
+//
+//  Created by Josh Reichman on 1/24/18.
+//  Copyright © 2018 Josh Reichman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func digitPressed(_ sender: UIButton) {
+    }
+    @IBAction func makeCall(_ sender: UIButton) {
+    }
+    
+}
+
