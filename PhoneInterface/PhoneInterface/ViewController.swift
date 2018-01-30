5deb94645d24f0a7834a5e8e8a8cc030eb60722d
Josh-Reichman <whodaman101@gmail.com>
2018-01-30 10:39:39 -0500

    Finished Phone Interface Homework Could not do part a (convert to valid phone number format)

--- PhoneInterface/PhoneInterface/ViewController.swift
+++ PhoneInterface/PhoneInterface/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var numberLabel: UILabel!
     @IBOutlet weak var callLabel: UIButton!
     @IBOutlet weak var deleteButton: UIButton!
+    @IBOutlet weak var hangupButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,28 +22,41 @@ class ViewController: UIViewController {
 
     @IBAction func digitPressed(_ sender: UIButton) {
         numberLabel.text?.append((sender.currentTitle)!)
-        if ((numberLabel.text?.count)! == 10){
-            callLabel.isEnabled = true
-        }
-        else {
-            callLabel.isEnabled = false
-        }
-
+        validateNumber()
     }
+    
     @IBAction func deleteDigit(_ sender: UIButton) {
         if ((numberLabel.text?.count)! != 0){
             var labelTemp : String! = numberLabel.text!
             labelTemp.removeLast()
             numberLabel.text = labelTemp
-            print(labelTemp!)
-            print("deleted")
+            validateNumber()
         }
     }
-    
     @IBAction func makeCall(_ sender: UIButton) {
-        let temp = 1
-        
+        numberLabel.text = "call initiated..."
+        hangupButton.isEnabled = true
+        deleteButton.isEnabled = false
+        callLabel.isEnabled = false
     }
-    
+    @IBAction func hangUp(_ sender: UIButton) {
+        numberLabel.text = ""
+        hangupButton.isEnabled = false
+        deleteButton.isEnabled = true
+    }
+    func validateChars() -> Bool {
+        return !((numberLabel.text?.contains("*"))! || (numberLabel.text?.contains("#"))!)
+    }
+    func checkDigitCount() -> Bool {
+        return((numberLabel.text?.count)! == 10)
+    }
+    func validateNumber(){
+        if (validateChars() && checkDigitCount()){
+            callLabel.isEnabled = true
+        }
+        else {
+            callLabel.isEnabled = false
+        }
+    }
 }
 
