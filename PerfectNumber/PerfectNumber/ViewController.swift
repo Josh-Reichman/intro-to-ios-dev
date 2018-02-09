3af02595939e2307333c3b9556dfd8982a240492
Josh-Reichman <whodaman101@gmail.com>
2018-02-09 12:20:36 -0500

    More Perfect number work

--- PerfectNumber/PerfectNumber/ViewController.swift
+++ PerfectNumber/PerfectNumber/ViewController.swift
@@ -5,7 +5,9 @@
 //  Created by Josh Reichman on 2/9/18.
 //  Copyright © 2018 Josh Reichman. All rights reserved.
 //
-
+/*
+ A perfect number is a positive integer that is equal to the sum of its proper positive divisors (e.g., 6 = 1+2+3). Develop an app that accepts a start integer and an end integer. Search for the first perfect number in between if there is one. You need to use UITextField to accept inputs and use UILabel to give your answer.
+ */
 import UIKit
 
 class ViewController: UIViewController {
@@ -13,16 +15,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var startField: UITextField!
     @IBOutlet weak var endField: UITextField!
     @IBOutlet weak var calculateButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.resignFirstResponder()
+        
+    }
+    //https://rosettacode.org/wiki/Perfect_numbers#Swift
+    func perfect(n:Int) -> Bool {
+        var sum = 0
+        for i in 1..<n {
+            if n % i == 0 {
+                sum += i
+            }
+        }
+        return sum == n
     }
 
-
 }
-
