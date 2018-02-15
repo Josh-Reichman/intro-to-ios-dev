50e3939cc7faf85507640a075df0de3f589fa401
Josh-Reichman <whodaman101@gmail.com>
2018-02-15 11:21:30 -0500

    Moved HW3 stuff to separate folder

---
+++ HW3/BlackjackHW3/Blackjack/BlackJackModel.swift
@@ -0,0 +1,116 @@
+//
+//  BlackJackModel.swift
+//  Blackjack
+//
+//  Created by Josh Reichman on 2/9/18.
+//  Copyright © 2018 Josh Reichman. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum Suit: Int {
+    case club = 0, spade, diamond, heart
+    func simpleDescription() -> String{
+        switch self {
+        case .club:
+            return "club"
+        case .spade:
+            return "spade"
+        case .diamond:
+            return "diamond"
+        case .heart:
+            return "heart"
+        }
+    }
+}
+
+class Card {
+    var suit: Suit = .club
+    var digit = 1
+    var isFaceUp = true
+    
+    init(suit: Suit, digit: Int) {
+        self.suit = suit
+        self.digit = digit
+    }
+    
+    static func generateDeck() -> [Card]{
+        var deckOfCards = [Card]()
+        for i in 0..<4{
+            for j in 1...13{
+                deckOfCards.append(Card(suit: Suit(rawValue: i)!, digit: j))
+            }
+        }
+        return deckOfCards
+    }
+    
+    func getCardImage() -> UIImage? {
+        if isFaceUp{
+            return UIImage(named: "\(suit.simpleDescription())-\(digit).png") //name-#.png
+        }
+        else{
+            return UIImage(named: "card-back.png") //card-back.png
+        }
+    }
+}
+
+class BlackJackGameModel {
+    private var cards = [Card]()
+    private var playerCards = [Card]()
+    private var dealerCards = [Card]()
+    
+    let maxPlayerCards = 5
+    
+    init() {
+        resetGame()
+    }
+    func shuffle(cards: [Card]) -> [Card]{
+        var copy2 = self.cards
+        var shuffledTemp = [Card]()
+        var cardTemp: Card
+        var rand: Int
+        for _ in 0..<52{
+            rand = Int(arc4random_uniform(UInt32(copy2.count)))
+            cardTemp = copy2.remove(at: rand)
+            shuffledTemp.append(cardTemp)
+        }
+        return shuffledTemp
+    }
+    public func getDeck() -> [Card]{
+        return cards
+    }
+    func resetGame() {
+        cards = Card.generateDeck()
+        
+        playerCards = [Card]()
+        dealerCards = [Card]()
+    }
+    func nextPlayerCard() -> Card{
+        let card = cards.removeFirst()
+        playerCards.append(card)
+        return card
+    }
+    func nextDealerCard() -> Card{
+        let card = cards.removeFirst()
+        dealerCards.append(card)
+        return card
+    }
+    func playerCardAtIndex(index:Int) -> Card?{
+        if index < playerCards.count{
+            return playerCards[index]
+        }
+        else{
+            return nil
+        }
+    }
+    func dealerCardAtIndex(index:Int) -> Card?{
+        if index < dealerCards.count{
+            return dealerCards[index]
+        }
+        else{
+            return nil
+        }
+    }
+}
+
---
+++ HW3/BlackjackHW3/Blackjack/BlackJackViewController.swift
@@ -0,0 +1,95 @@
+//
+//  BlackJackViewController.swift
+//  Blackjack
+//
+//  Created by Josh Reichman on 2/9/18.
+//  Copyright © 2018 Josh Reichman. All rights reserved.
+//
+
+import UIKit
+class BlackJackViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet var dealerCards: [UIImageView]!
+    @IBOutlet var playerCards: [UIImageView]!
+    @IBOutlet weak var numOfDecksField: UITextField!
+    @IBOutlet weak var generateDecksButton: UIButton!
+    
+    var gameModel: BlackJackGameModel
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        gameModel = BlackJackGameModel()
+        super.init(coder: aDecoder)
+    }
+    
+    func restartGame(){
+        gameModel.resetGame()
+        var card = gameModel.nextDealerCard()
+        card = gameModel.nextDealerCard()
+        card.isFaceUp = false
+        
+        card = gameModel.nextPlayerCard()
+        card = gameModel.nextPlayerCard()
+        renderCards()
+    }
+    
+    func renderCards() {
+        for i in 0 ..< gameModel.maxPlayerCards {
+            if let dealerCard = gameModel.dealerCardAtIndex(index:i){
+                dealerCards[i].image = dealerCard.getCardImage()
+            }
+            else{
+                dealerCards[i].isHidden = true
+            }
+            
+            if let playerCard = gameModel.playerCardAtIndex(index:i){
+                playerCards[i].image = playerCard.getCardImage()
+            }
+            else{
+                playerCards[i].isHidden = true
+            }
+        }
+    }
+    
+    @IBAction func userClickGenerate(_ sender: UIButton) {
+        var decks = [[Card]]()
+        for _ in 0 ..< Int(numOfDecksField.text!)!{
+            decks.append(Card.generateDeck())
+        }
+        
+    }
+    @IBAction func userClickShuffle(_ sender: UIButton) {
+        
+        var decksCopy = gameModel.getDeck()
+        var deckShuffled = [[Card]]()
+        for i in 0 ..< Int(numOfDecksField.text!)!{
+            deckShuffled.append(gameModel.shuffle(cards: [decksCopy[i]]) )
+        }
+        for i in 0 ..< deckShuffled.count{
+            printDeck(deckNumber: i+1, deck: deckShuffled[i])
+        }
+    }
+    func printDeck(deckNumber: Int, deck:[Card]){
+        var output = "Deck Number \(deckNumber) \n\n"
+        for i in 0 ..< 52{
+            output.append("Card: \(i+1) Suit: \(deck[i].suit.simpleDescription()) Value: \(deck[i].digit.description)\n")
+        }
+        print(output)
+    }
+    @IBAction func userClickStand(_ sender: UIButton) {
+    }
+    @IBAction func userClickHit(_ sender: UIButton) {
+    }
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if(numOfDecksField.text == "" || (Int(numOfDecksField.text!)!) <= 0){
+            numOfDecksField.text = "1"
+        }
+        numOfDecksField.resignFirstResponder()
+    }
+}
+
