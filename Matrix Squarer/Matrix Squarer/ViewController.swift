7e3a533699c79da1afb4d08c91ae86ee9275bca8
Josh-Reichman <whodaman101@gmail.com>
2018-02-21 09:17:45 -0500

    Finished Main Functions for MatrixSquarer

--- Matrix Squarer/Matrix Squarer/ViewController.swift
+++ Matrix Squarer/Matrix Squarer/ViewController.swift
@@ -18,6 +18,27 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    func string2MatrixArray(_ Input: String){
+        var temp = Input.components(separatedBy: ";")
+        var temp2 = Array<[String]>()
+        for i in 0..<temp.count{
+            temp2.append(temp[i].components(separatedBy: " "))
+        }
+        outputLabel.text = squareString(temp2)
+    }
+    
+    private func squareString(_ Input: [[String]]) -> String{
+        var output = ""
+        for m in 0..<Input.count{
+            for n in 0..<Input[m].count{
+                let temp = Float(Input[m][n])
+                output.append(String(temp!*temp!) + ", ")
+            }
+            output.removeLast(2)
+            output.append(";")
+        }
+        return output
+    }
     
 }
 
