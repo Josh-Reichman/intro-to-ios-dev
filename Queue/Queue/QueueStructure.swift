a399d046f3e5df5abaaf6525fc48096388af767d
Josh-Reichman <whodaman101@gmail.com>
2018-02-23 14:38:03 -0500

    Created Quest Structure

---
+++ Queue/Queue/QueueStructure.swift
@@ -0,0 +1,40 @@
+//
+//  QueueStructure.swift
+//  Queue
+//
+//  Created by Josh Reichman on 2/23/18.
+//  Copyright © 2018 Josh Reichman. All rights reserved.
+//
+
+import Foundation
+
+public struct Queue<T> {
+    var list = [T]()
+
+    mutating func enqueue(_ element: T) {
+        list.append(element)
+    }
+    mutating func dequeue() -> T? {
+        if !list.isEmpty {
+            return list.removeFirst()
+        } else {
+            return nil
+        }
+    }
+    
+    func peek() -> T? {
+        if !list.isEmpty {
+            return list[0]
+        } else {
+            return nil
+        }
+    }
+    
+    var count: Int {
+        return list.count
+    }
+    
+    var isEmpty: Bool {
+        return list.isEmpty
+    }
+}
